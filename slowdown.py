42d62877f74a1231ed5511e3409a3b522b070f67
fufjvnvnf <fufjvnvnf@gmail.com>
2019-12-04 21:24:26 -0500

    utils

---
+++ slowdown.py
@@ -0,0 +1,54 @@
+import argparse
+import numpy as np
+from matplotlib import pyplot
+
+bd1 = 40
+bd2 = 160
+latency = 200
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--input')
+    args = parser.parse_args()
+
+    slowdowns = []
+    with open(args.input, 'r') as f:
+        for line in f.readlines():
+            tokens = line.split()
+            i = ipToNode(tokens[0])
+            j = ipToNode(tokens[1])
+            size = int(tokens[4])*8
+            slowdowns.append(int(tokens[6])/oracle(i,j,size))
+    
+    slowdowns = np.array(slowdowns)
+
+    slowdowns.sort()
+
+    s = 0
+    i = 0
+    dx = 0.01
+    X = np.arange(1, 100, dx)
+    Y = []
+    for s in X:
+        while i<len(slowdowns) and slowdowns[i]<s:
+            i+=1
+        Y.append(i)
+
+    Y = np.array(Y)
+    Y=Y/len(slowdowns)
+    pyplot.plot(X,Y)
+    pyplot.xscale('log')
+    pyplot.show()
+
+def ipToNode(ip):
+    return (int(ip, 16)-int('0b000001', 16))/int('00000100', 16)
+
+
+def oracle(i, j, size):
+    if i/16 == j/16:
+        return size/bd1+2*latency
+    return size/bd1+4*latency
+
+
+if __name__ == '__main__':
+    main()
