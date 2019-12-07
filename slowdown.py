e5997d6144a0a85730736e77f41947cbb04ef6a6
fufjvnvnf <fufjvnvnf@gmail.com>
2019-12-07 01:52:43 -0500

    port

--- slowdown.py
+++ slowdown.py
@@ -6,6 +6,7 @@ bd1 = 40
 bd2 = 160
 latency = 200
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--input')
@@ -15,11 +16,9 @@ def main():
     with open(args.input, 'r') as f:
         for line in f.readlines():
             tokens = line.split()
-            i = ipToNode(tokens[0])
-            j = ipToNode(tokens[1])
-            size = int(tokens[4])*8
-            slowdowns.append(int(tokens[6])/oracle(i,j,size))
-    
+            slowdowns.append(
+                int(tokens[6])/oracle(tokens[0], tokens[1], tokens[4]))
+
     slowdowns = np.array(slowdowns)
 
     slowdowns.sort()
@@ -30,22 +29,25 @@ def main():
     X = np.arange(1, 100, dx)
     Y = []
     for s in X:
-        while i<len(slowdowns) and slowdowns[i]<s:
-            i+=1
+        while i < len(slowdowns) and slowdowns[i] < s:
+            i += 1
         Y.append(i)
 
     Y = np.array(Y)
-    Y=Y/len(slowdowns)
-    pyplot.plot(X,Y)
+    Y = Y/len(slowdowns)
+    pyplot.plot(X, Y)
     pyplot.xscale('log')
     pyplot.show()
+
 
 def ipToNode(ip):
     return (int(ip, 16)-int('0b000001', 16))/int('00000100', 16)
 
 
+# i,j are hex ip, size is in bytes
 def oracle(i, j, size):
-    if i/16 == j/16:
+    size = int(size)*8
+    if ipToNode(i)/16 == ipToNode(j)/16:
         return size/bd1+2*latency
     return size/bd1+4*latency
 
