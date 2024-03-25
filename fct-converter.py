ec616b1c3dfaaabf99f657137f89161a4610b321
shouxulin <1186849403@qq.com>
2024-03-25 16:26:06 -0400

    add throughput in fct converter

--- fct-converter.py
+++ fct-converter.py
@@ -1,11 +1,11 @@
 import sys
 
 INPUT_FILE = sys.argv[1]
-OUTPUT_FILE = sys.argv[2]
-outfile = open(OUTPUT_FILE,'w')
+# OUTPUT_FILE = sys.argv[2]
+# outfile = open(OUTPUT_FILE,'w')
 
 pktsize = 1500
-link_bandwidth = int(sys.argv[3])
+link_bandwidth = int(sys.argv[2])
 NUM_HOSTS_PER_AGG = 16
 propagation_delay_in_ns = 200
 
@@ -21,8 +21,10 @@ def idealTime(pktsize, link_bandwidth, NUM_HOSTS_PER_AGG, propagation_delay_in_n
         ideal_header_time = 2.0 * (8.0 * 40 / link_bandwidth)/1e3 + 2.0 * (8.0 * 40 / (4 * link_bandwidth))/1e3  + (4 * propagation_delay_in_ns * 1e-3)
 
     ideal_fct = ideal_header_time + ideal_data_time
+    # print("\theader: " + str(ideal_header_time*1e3) + "ns, data: " + str(ideal_data_time*1e3)) + "ns"
     return ideal_fct
 
+xput_all = []
 with open(INPUT_FILE) as f1:
     for line in f1:
         line_str = line.split()
@@ -31,5 +33,12 @@ with open(INPUT_FILE) as f1:
         flowsize = int(line_str[4])
         fct = float(line_str[6])/1e3
         ideal_fct = idealTime(pktsize, link_bandwidth, NUM_HOSTS_PER_AGG, propagation_delay_in_ns, flowsize, src, dst)
-        new_line = str(src) + ' ' + str(dst) + ' ' + str(float(line_str[5])/1e9) + ' ' + str(flowsize) + ' ' + str(fct) + ' ' + str(ideal_fct) + ' ' + str(fct/ideal_fct)
-        outfile.write(new_line + '\n')
+        
+        xput = (flowsize*8.0) / fct / 1e3
+        xput_all.append(xput)
+        ideal_xput = (flowsize*8.0) / ideal_fct / 1e3
+        
+        new_line = "sender: " + str(src) + ', receiver: ' + str(dst) + ', start time(s): ' + str(float(line_str[5])/1e9) + ', flowsize: ' + str(flowsize) + ' fct: ' + str(fct*1e3) + 'ns, ideal fct: ' + str(ideal_fct*1e3) + "ns, actual/ideal: " + str(fct/ideal_fct) + " xput: " + str(xput) + " ideal xput: " + str(ideal_xput)
+        print(new_line)
+        # outfile.write(new_line + '\n')
+print("avg xput: " + str(sum(xput_all)/len(xput_all)))
